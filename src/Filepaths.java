e484ed21ceed4c370b03ed04565053b7cc0e7614
ainacteru <gbenawijr@gmail.com>
2025-09-17 21:54:32 -0500

    forgot to press ctrl+s

--- src/Filepaths.java
+++ src/Filepaths.java
@@ -1,7 +1,7 @@
 import java.io.File;
 
 public class Filepaths {
-    public static final String GROSSTORIO_SPLASH_SCREEN_PATH = new File(System.getProperty("user.dir")).getParent() + "/splash/splash.png";
+    public static final String GROSSTORIO_SPLASH_SCREEN_PATH = new File(System.getProperty("user.dir")).getParent() + "graphics/splash/splash.png";
 
     public static final String FACTORIO_SPLASH_SCREEN_PATH = "/data/core/graphics/splash-screen-image.png";
 
