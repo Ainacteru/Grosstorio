2978e9e44004586f8120498d54571f2f0be44374
ainacteru <gbenawijr@gmail.com>
2025-10-04 10:20:59 -0500

    almost works!

--- src/FileManager.java
+++ src/FileManager.java
@@ -1,57 +1,52 @@
-import java.io.Console;
 import java.io.IOException;
 import java.nio.file.*;
 import java.util.List;
 
+
 public class FileManager {
 
     public void ReplaceSplash(Path newSplash, Path SplashToReplace) {
-        Path _newSplash = newSplash;
-        Path _newSplashCopy = newSplash.getParent().resolve("splash-screen-image.png"); // default name of factorio splash screen
-
-        Path _splashToReplace = SplashToReplace;
-        Path _splashToReplace_copy = SplashToReplace.getParent().resolve("splash-screen-image-default-copy.png");
+        Path backup = SplashToReplace.getParent().resolve("splash-screen-image-default-backup.png");
+        System.out.println(backup);
 
         try {
-            Files.copy(_newSplash, _newSplashCopy, StandardCopyOption.REPLACE_EXISTING);
-            Files.copy(_splashToReplace, _splashToReplace_copy, StandardCopyOption.REPLACE_EXISTING);
+            // Backup original splash before replacing
+            if (!Files.exists(backup)) {
+                Files.copy(SplashToReplace, backup, StandardCopyOption.REPLACE_EXISTING);
+                System.out.println("Backup created: " + backup);
+            }
 
-            System.out.println();
-            System.out.println("Made a copy of " + _newSplash + "and made a backup of " + _splashToReplace);
-
-            Files.createDirectories(_splashToReplace.getParent());
-            Files.move(_newSplashCopy, _splashToReplace, StandardCopyOption.REPLACE_EXISTING);
-
-            System.out.println();
-            System.out.println("Replaced original splash with " + _newSplashCopy);
+            // Replace the splash
+            Files.copy(newSplash, SplashToReplace, StandardCopyOption.REPLACE_EXISTING);
+            System.out.println("Replaced splash with: " + newSplash);
 
         } catch (IOException ex) {
         }
     }
 
-    public void AddMod(Path mods, Path FACTORIO_PATH) {
+    public void AddMod(Path mods) {
         Path _modsToTransfer = mods;
-        Path destination = FACTORIO_PATH;
-
-
 
         try {
-            Files.write(Filepaths.MODS_TXT, "".getBytes(), StandardOpenOption.TRUNCATE_EXISTING);
+            Files.write(Filepaths.Config.MODS_TXT, "".getBytes(), StandardOpenOption.TRUNCATE_EXISTING);
         } catch (IOException ex) {}
         //clear then write
 
         try {
             DirectoryStream<Path> _mods = Files.newDirectoryStream(_modsToTransfer, "*.zip");
+
+            long count = Files.list(_modsToTransfer).filter(p -> p.toString().endsWith(".zip")).count();
+            System.out.println();
+            System.out.println("Found " + count + " mods to add");
+
             for (Path mod : _mods) {
-                Path modToMove = mod;
 
                 //System.out.println("Found: " + mod.getFileName());
-                Files.write(Filepaths.MODS_TXT, (mod.getFileName().toString() + System.lineSeparator()).getBytes(), StandardOpenOption.CREATE, StandardOpenOption.APPEND);
+                Files.write(Filepaths.Config.MODS_TXT, (mod.getFileName().toString() + System.lineSeparator()).getBytes(), StandardOpenOption.CREATE, StandardOpenOption.APPEND);
 
-                System.out.println("Added " + mod.getFileName() + " to " + Filepaths.MODS_TXT.toAbsolutePath());
+                System.out.println("Added " + mod.getFileName() + " to " + Filepaths.Config.MODS_TXT.toAbsolutePath());
 
-
-                Files.copy(mod, Filepaths.Factorio.FACTORIO_MODS_PATH.resolve(mod.getFileName()), StandardCopyOption.REPLACE_EXISTING);
+                Files.copy(mod, Filepaths.Factorio.getModsPath().resolve(mod.getFileName()), StandardCopyOption.REPLACE_EXISTING);
                 //Files.move(modToMove, mod, options)
                 //Files.c
             }
@@ -62,13 +57,13 @@ public class FileManager {
 
     public void RemoveGrosstorioMods() {
         try {
-            Path mods = Filepaths.MODS_TXT;
+            Path mods = Filepaths.Config.MODS_TXT;
             
             List<String> txt = Files.readAllLines(mods);
 
             for (String line : txt) {
                 //System.out.println(line);
-                Path mod = Filepaths.Factorio.FACTORIO_MODS_PATH.resolve(line);
+                Path mod = Filepaths.Factorio.getModsPath().resolve(line);
 
                 System.out.println("found mod to delete: " + mod);
 
@@ -81,17 +76,32 @@ public class FileManager {
 
     }
 
-    //replace the splash screen, then delete it -- i could also just mv it to rename it and replace it but i'll do this for simplicity
     public void RevertToDefaultSplash() {
-        ReplaceSplash(Filepaths.Factorio.FACTORIO_SPLASH_SCREEN_PATH_BACKUP, Filepaths.Factorio.FACTORIO_SPLASH_SCREEN_PATH);
+        Path backup = Filepaths.Factorio.getSplashScreenBackupPath();
 
         try {
-            Files.delete(Filepaths.Factorio.FACTORIO_SPLASH_SCREEN_PATH_BACKUP);
-
-            System.out.println();
-            System.out.println("DELETED " + Filepaths.Factorio.FACTORIO_SPLASH_SCREEN_PATH_BACKUP);
-            
+            Files.delete(backup);
+            System.out.println("Deleted backup: " + backup);
         } catch (IOException ex) {
+            System.out.println("Could not delete backup (already removed?): " + ex.getMessage());
         }
     }
+
+
+    public void RunGame() {
+        try {
+            ProcessBuilder pb = new ProcessBuilder(Filepaths.Factorio.getFactorioBinary().getAbsolutePath());
+            pb.directory(Filepaths.Factorio.getFactorioPath().toFile());
+            
+            Process gameProcess = pb.start();
+            System.out.println("Factorio started.");
+
+            int exitCode = gameProcess.waitFor();
+            System.out.println("Factorio closed with exit code: " + exitCode);
+
+        } catch (IOException | InterruptedException ex) {
+        }
+    }
+
+
 }
--- src/Filepaths.java
+++ src/Filepaths.java
@@ -1,33 +1,116 @@
-import java.io.IOException;
 import java.nio.file.*;
+import javax.swing.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
 
 public class Filepaths {
 
+    public static void SetupFilepaths() {
 
-    public static String FACTORIO_PATH = "/Factorio";
+            
+        try {
+            List<String> path = Files.readAllLines(Config.PATHS_TXT);
 
-    // home/user/Applications/Games/Factorio/data/core/graphics/splash-screen-image.png 
-    // my file location
-    // Paths.get(FACTORIO_PATH).resolve("data").resolve("core").resolve("graphics").resolve("splash-screen-image.png");
+            // for (String elem : path) {
+            //     System.out.println(elem);
+            // }
 
-    public static final Path MODS_TXT = Paths.get(System.getProperty("user.dir")).getParent().resolve("mods").resolve("mods.txt");
+
+            if(path.isEmpty()) {
+
+                JFileChooser chooser = new JFileChooser();
+
+                chooser.setDialogTitle("Select the Factorio Directory Location");
+                chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+                
+                chooser.setCurrentDirectory(new File(System.getProperty("user.home")));
+
+                int result = chooser.showOpenDialog(null);
+
+                if (result == JFileChooser.APPROVE_OPTION) {
+                    Factorio.setFactorioPath(Paths.get(chooser.getSelectedFile().getAbsolutePath()));
+
+
+                    System.out.println("Factorio directory is at: " + Factorio.FACTORIO_PATH);
+                } else {
+                    System.out.println("Selection canceled");
+                }
+            }
+            else {
+                Factorio.setFactorioPath(Paths.get(path.getFirst()));
+                //System.out.println(Paths.get(path.getFirst()));
+            }
+        } catch (IOException ex) {}
+    }
+
+
+    public static class Config {
+            public static final Path MODS_TXT = Paths.get(System.getProperty("user.dir")).getParent().resolve("config").resolve("Mods.txt");
+            public static final Path PATHS_TXT = Paths.get(System.getProperty("user.dir")).getParent().resolve("config").resolve("Paths.txt");
+    }
 
     public static class Grosstorio {
 
-        public static final Path GROSSTORIO_SPLASH_SCREEN_PATH = Paths.get(System.getProperty("user.dir")).getParent().resolve("graphics").resolve("splash").resolve("splash.png");
+        public static Path getSplashPath() {
+            return Paths.get(System.getProperty("user.dir"))
+                        .getParent()
+                        .resolve("graphics")
+                        .resolve("splash")
+                        .resolve("splash.png");
+        }
 
-        public static final Path GROSSTORIO_MODS_PATH = Paths.get(System.getProperty("user.dir")).getParent().resolve("mods");
-
+        public static Path getModsPath() {
+            return Paths.get(System.getProperty("user.dir"))
+                        .getParent()
+                        .resolve("mods");
+        }
     }
 
     public static class Factorio {
-        
-        public static final Path FACTORIO_SPLASH_SCREEN_PATH = Paths.get(System.getProperty("user.dir")).getParent().resolve("factorio").resolve("data").resolve("core").resolve("graphics").resolve("splash-screen-image.png");
+        private static Path FACTORIO_PATH;
 
-        public static final Path FACTORIO_SPLASH_SCREEN_PATH_BACKUP = Paths.get(System.getProperty("user.dir")).getParent().resolve("factorio").resolve("data").resolve("core").resolve("graphics").resolve("splash-screen-image-default-backup.png");
+        public static void setFactorioPath(Path path) {
+            FACTORIO_PATH = path;
+            try {
+                Files.write(Config.PATHS_TXT, FACTORIO_PATH.toString().getBytes());
+            } catch (IOException ex) {
+            }
+        }
 
-        public static final Path FACTORIO_MODS_PATH = Paths.get(System.getProperty("user.dir")).getParent().resolve("factorio").resolve("mods");
+        public static Path getFactorioPath() {
+            return FACTORIO_PATH;
+        }
 
+        public static File getFactorioBinary() {
+            File binary = getFactorioPath().resolve("bin")
+                                            .resolve("x64")
+                                            .resolve("factorio")
+                                            .toFile();
 
+            return binary;
+
+        }
+
+        public static Path getSplashScreenPath() {
+            return FACTORIO_PATH.resolve("data")
+                                .resolve("core")
+                                .resolve("graphics")
+                                .resolve("splash-screen-image.png");
+        }
+
+        public static Path getSplashScreenBackupPath() {
+            return FACTORIO_PATH.resolve("data")
+                                .resolve("core")
+                                .resolve("graphics")
+                                .resolve("splash-screen-image-default-backup.png");
+        }
+
+        public static Path getModsPath() {
+            return FACTORIO_PATH.getParent()
+                                .resolve("factorio")
+                                .resolve("mods");
+        }
     }
+
 }   
